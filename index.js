47f10a18661793437d302c07dc5b816da1e18d88
Nathan Nichols <nnichols@concentricsky.com>
2018-10-19 09:38:43 -0700

    clean up reporting

--- index.js
+++ index.js
@@ -31,12 +31,7 @@ var avg2 = lo.sum(uniqs.slice(0, uniqs.length-1)) / uniqs.length - 1;
 const num = n => Number(n).toLocaleString();
 
 console.log(chalk`
-Wow — you gave me {red ${num(string.length)}} non-orthographic characters! 
-That's a total of {red ${num(graphemes.length)}} orthographic graphemes. 
-We found {red ${uniq_g.size}} unique orthographic graphemes. 
-We split these graphemes up into {red ${num(chunks.length)}} total chunks. 
-(the last chunk size is {red ${num(chunks.map(c => c.length)[chunks.length-1])}}) 
-Each chunk on average has {red ${num(avg)}} unique graphemes. 
-If we ignore the last chunk the average unique grapheme in each chunk would be {red ${num(avg2)}}. 
+Wow — you gave me {red ${num(string.length)}} characters! That's a total of {red ${num(graphemes.length)}} orthographic graphemes (and things like " " and "\\n"). (we found {red ${uniq_g.size}} total unique orthographic graphemes) We split the graphemes up into {red ${num(chunks.length)}} total chunks. (the last chunk size is {red ${num(chunks.map(c => c.length)[chunks.length-1])}}) Each chunk on average has {red ${num(avg)}} unique graphemes. If we ignore the last chunk the average unique grapheme count in each chunk would be {red ${num(avg2)}}. 
 
-Thanks for crunching letters with us today!`);
+{bold Thanks for crunching letters with us today!}
+`);
